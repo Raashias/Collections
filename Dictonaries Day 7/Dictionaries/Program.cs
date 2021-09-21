2b4d7cbc4b55de2d1715cc670cb1df812dc3df61
Raashias <raashias@gmail.com>
2021-09-21 17:29:48 +0530

    Updated Day 7 assignment

---
+++ Dictonaries Day 7/Dictionaries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IDictionary<int, string> Game = new Dictionary<int, string>();
+            Game.Add(1, "Player 5");
+            Game.Add(2, "Player 1");
+            Game.Add(3, "Player 3");
+            Game.Add(4, "Player 2");
+            Game.Add(5, "Player 4");
+            Console.WriteLine("Total number of players are: " + Game.Keys.Count);
+
+            foreach (KeyValuePair<int, string> k in Game)
+                Console.WriteLine("Key: {0}, Value: {1}", k.Key, k.Value);
+            Console.WriteLine();
+            Console.WriteLine("New Results are as follows");
+            Game[1] = "Player 4"; //Updatin the data using key value
+            Game[5] = "Player 5";
+
+            foreach (KeyValuePair<int, string> g in Game)
+                Console.WriteLine("Key: {0}, Value: {1}", g.Key, g.Value);
+
+            Game.Clear();
+
+
+            Console.Read();
+        }
+    }
+}
